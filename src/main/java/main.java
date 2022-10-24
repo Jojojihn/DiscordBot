09d8c4198e0ef090fb7049587080ac11057eee66
Jojojihn <jojojihn31@gmail.com>
2022-10-24 15:03:53 +0200

    First "working" build

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,21 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.managers.Presence;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class main {
-    public static void main(String args[]){
+    public static void main(String[] args) throws Exception {
+        URL tokenUrl = main.class.getResource("/token.txt");
+        String token = Files.readString(Paths.get(tokenUrl.toURI()));
+        JDA bot = JDABuilder.createDefault(token).build();
+        Presence presence = bot.getPresence();
+        presence.setActivity(Activity.watching("you"));
+
+
 
     }
 }
