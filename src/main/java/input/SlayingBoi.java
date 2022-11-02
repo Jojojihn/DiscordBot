1c775337448e2cfd843ebbf63a600335ff9786ee
Jojojihn <jojojihn31@gmail.com>
2022-11-02 08:59:10 +0100

    Small improvement
    DId a regex with a small improvement

--- src/main/java/input/SlayingBoi.java
+++ src/main/java/input/SlayingBoi.java
@@ -4,43 +4,8 @@ import net.dv8tion.jda.api.entities.Message;
 
 public class SlayingBoi {
     public boolean slay(Message message) {
-        if (message.getContentRaw().toLowerCase().startsWith("s")) {
-            char[] chars = message.getContentRaw().toLowerCase().toCharArray();
-            int i = 0;
-            boolean breaker = false;
-            while (chars[i] == 's' && !breaker) {
-                i += 1;
-                if (chars.length == i) {
-                    breaker = true;
-                    i -= 1;
-                }
-                if (chars[i] == 'l') {
-                    while (chars[i] == 'l' && !breaker) {
-                        i += 1;
-                        if (chars.length == i) {
-                            breaker = true;
-                            i -= 1;
-                        }
-                        if (chars[i] == 'a') {
-                            while (chars[i] == 'a' && !breaker) {
-                                i += 1;
-                                if (chars.length == i) {
-                                    breaker = true;
-                                    i -= 1;
-                                }
-                                if (chars[i] == 'y') {
-                                    while (chars[i] == 'y' && !breaker) {
-                                        i += 1;
-                                        if (chars.length == i) {
-                                            return true;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        if (message.getContentRaw().toLowerCase().matches("s+l+a+y+")) {
+            return true;
         }
         return false;
     }
